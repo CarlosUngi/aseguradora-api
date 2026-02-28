f90ef00d7342ec8499175b54f613d0622b6264f3
carlosGarzon <carlos.ungi@gmail.com>
2026-02-28 11:16:57 -0500

    inicializate database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- Inicialización de Tipos de Póliza (Tabla maestra)
+INSERT INTO policy_types (id, nombre, observaciones) VALUES (1, 'Vida', 'Seguro de vida individual');
+INSERT INTO policy_types (id, nombre, observaciones) VALUES (2, 'Vehículo', 'Seguro para vehículos particulares');
+INSERT INTO policy_types (id, nombre, observaciones) VALUES (3, 'Salud', 'Cobertura médica integral');
+
